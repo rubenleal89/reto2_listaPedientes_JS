304c1641504e676613897b01757d7fb8214dc080
rubenleal89 <rubendarioleal618@gmail.com>
2022-09-01 22:33:03 -0500

    feat fix function order

--- code.js
+++ code.js
@@ -5,7 +5,6 @@ function crearItem(contador,nameTarea){
     const section = document.querySelector("#sect-lista");
 // Creacion del div que contiene divTarea y divButton
     let divGlobalE = document.createElement("div");
-    divGlobalE.setAttribute("idDivGlobal",`${contador}`); // Revisar si es necesario
 // Creacion del div que contiene el CHECKBOX y el texto de la TAREA
     let divTarea = document.createElement("div");
     divTarea.className="div-check-tarea";
@@ -14,7 +13,6 @@ function crearItem(contador,nameTarea){
     checkbox.className="checkSeleccionar";
     let parrafoNameTarea = document.createElement("p");
     parrafoNameTarea.className="txtEdit";
-    parrafoNameTarea.id= `${contador}`;
     parrafoNameTarea.textContent=`${nameTarea}`;
 // creacion del div que contiene los BOTONES: Editar y Eliminar
     let divButton = document.createElement("div");
@@ -22,16 +20,13 @@ function crearItem(contador,nameTarea){
     let buttonEdit = document.createElement("button");
     buttonEdit.textContent = "Edit";
     buttonEdit.className="btnEdit";
-    buttonEdit.id=`${contador}`;
-    buttonEdit.addEventListener("click",(edita) 
+    buttonEdit.addEventListener("click",(editInput) 
         // console.log(e);
         // console.log(e.target.parentNode.parentNode.childNodes[1]);
     );
     let buttonDelete = document.createElement("button");
     buttonDelete.textContent = "Delete";
     buttonDelete.className="btnDelete";
-    buttonDelete.setAttribute("idBtnDelete",`btnDelete${contador}`); // Revisar si es necesario
-    // buttonDelete.type="button";
     buttonDelete.addEventListener("click",(e) => {
         eliminar(buttonDelete);
     }
@@ -39,7 +34,6 @@ function crearItem(contador,nameTarea){
 // Creacion de las insercion de los div a la seccion
     section.insertAdjacentElement("beforeend",divGlobalE)
     divGlobalE.insertAdjacentElement("beforeend",divTarea);
-    // divGlobalE.insertAdjacentElement("beforeend",divButton);
 // Creacion de las inserciones de elementos a los Div
     divTarea.insertAdjacentElement("afterbegin",checkbox);
     divTarea.insertAdjacentElement("beforeend",parrafoNameTarea);
@@ -51,7 +45,7 @@ function crearItem(contador,nameTarea){
 let numTasks = 0;
 function contador(e,botonEl){
     let parrafoCounterTasks = document.getElementById("numTasks");
-    if(e.type){
+    if(e.type){ // (e.type) me hace saber si la funcion se ejecuto
         numTasks += 1
         parrafoCounterTasks.textContent = `${numTasks} tasks remaining`; // si es necesario
     }
@@ -61,35 +55,40 @@ function contador(e,botonEl){
     }
 }
 
+function limpiarFormulario() {
+    document.getElementById("tarea").value = '';
+}
+
 function eliminar(botonEl) {
     botonEl.parentNode.parentNode.remove();
     contador("",botonEl)
 }
 
-
-
-// formulario.addEventListener("submit", editar);
-let acesoP;
-function edita(botonEd){ // Al hacer clip en editar, el valor de P pasa al Input
+let accesoNameTarea;
+function editInput(botonEd){ // Al hacer clip en editar, el valor de P pasa al Input
     balidarBtn = false;
-    acesoP = botonEd.target.parentNode.parentNode.childNodes[1];
-    let valorElem = acesoP.textContent;
+    accesoNameTarea = botonEd.target.parentNode.parentNode.childNodes[1]; // llamar al elemento P del nombre tarea
+    let valorElem = accesoNameTarea.textContent;
     let inputEdi = document.getElementById("tarea");
     inputEdi.value=valorElem;
     let btnUpdate = document.querySelector("#btnAdd"); // llamado del boton Add y cambiar a Editar
     btnUpdate.value="Editar";
-    // let tareaEdit = document.querySelector(".txtEdit"); // llamado de parrafoNameTarea
-    // tareaEdit.textContent;
 }
 
-function limpiarFormulario() {
-    document.getElementById("tarea").value = '';
+function updateTarea(){
+    balidarBtn = true;
+    let inpuEdit = document.getElementById("tarea").value;
+    if(inpuEdit === ""){
+        alert("Campos vacio, por favor ingresar un valor")
+    }
+    else{
+        accesoNameTarea.textContent= `${inpuEdit}`;
+        
+    }
+    let btnUpdate = document.querySelector("#btnAdd"); // llamado del boton Add y cambiar a Editar
+    btnUpdate.value="Add";
+    limpiarFormulario();
 }
-
-
-
-let formulario = document.getElementById("formTarea");
-formulario.addEventListener("submit", condi);
 
 function envio(e){
     let tareaPendiente = document.getElementById("tarea").value;
@@ -99,27 +98,14 @@ function envio(e){
     balidarBtn = true;
 }
 
-function editarSubmit (){
-    balidarBtn = true;
-    console.log(acesoP);
-    let inpuEdit = document.getElementById("tarea").value;
-    if(inpuEdit === ""){
-        alert("Campos vacio, por favor ingresar un valor")
-    }
-    else{
-        acesoP.textContent= `${inpuEdit}`; // innerHTML 
-        
-    }
-    let btnUpdate = document.querySelector("#btnAdd"); // llamado del boton Add y cambiar a Editar
-    btnUpdate.value="Add";
-}
-
-function condi(e){
+let formulario = document.getElementById("formTarea");
+formulario.addEventListener("submit", onFunctions);
+function onFunctions(e){
     e.preventDefault();
     if(balidarBtn===true){
         envio(e);
     }
     else if(balidarBtn===false){
-        editarSubmit(e);
+        updateTarea(e);
     }
 }
